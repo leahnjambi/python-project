b774c5df42563e582a0209a2467853f59f3d42ae
leahnjambi <njambileah07@gmail.com>
2023-03-02 10:50:01 +0300

    input some products and connecting my python project to django

--- morningDjangoProject/settings.py
+++ morningDjangoProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'crispy_bootstrap4',
+    'morningDjangoProject',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,9 @@ STATICFILES_DIRS = [
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- morningDjangoProject/views.py
+++ morningDjangoProject/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+from .models import Product
+
 
 
 def register(request):
@@ -14,3 +17,21 @@ def register(request):
         form = UserRegistrationForm()
 
     return render(request, 'register.html', {'form': form})
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
+def add_product(request):
+    if request.method == 'POST':
+        p_name = request.POST.get("jina")
+        p_quantity = request.POST.get("kiasi")
+        p_price = request.POST.get("bei")
+        product = Product(prod_name=p_name, prod_quantity=p_quantity, prod_price=p_price)
+
+        product.save()
+        messages.success(request, 'product saved successfully')
+        return redirect('add-product')
+    return render(request, 'add product.html')
